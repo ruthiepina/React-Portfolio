c18cbf62cfc0a1a9c1412b8340a1053c0a874b38
RMP <ruthiepina@gmail.com>
2024-04-09 19:47:28 -0600

    Added header and footer container react code, not sure on class name/styling for footer. Just including link to my github for time sake / running behind!!!

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+   return (
+      <footer className="footer-container">
+         <a href="https://github.com/ruthiepina" target="_blank" rel="noopener noreferrer"></a>
+         <a href="https://github.com/ruthiepina" target="_blank" rel="noopener noreferrer"></a>
+         <a href="https://github.com/ruthiepina" target="_blank" rel="noopener noreferrer"></a>
+         <a href="https://github.com/ruthiepina" target="_blank" rel="noopener noreferrer"></a>
+      </footer>
+   );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
+import "./Header.css";
+
+function Header() {
+   return (
+      <header>
+         <h1>Ruthie Pina</h1>
+         <Nav />
+      </header>
+   );
+}
+
+export default Header;
