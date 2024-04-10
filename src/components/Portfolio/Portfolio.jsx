d011c0b1e20f8184c0ae230d285356f4d552d2b6
RMP <ruthiepina@gmail.com>
2024-04-09 23:53:02 -0600

    added images and fixed link to my portfolio items. VERY UGY APP BUT IT"S WORKING!!!!!!

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -22,42 +22,42 @@ const Portfolio = () => {
          id: 1,
          title: "Coding Quiz",
          description: "Click to see the repo.",
-         imageUrl: "",
+         imageUrl: "../../../public/images/codingquiz.png",
          githubUrl: "https://github.com/ruthiepina/Coding-Quiz",
       },
       {
          id: 2,
          title: "Tech Blog",
          description: "Click to see the repo",
-         imageUrl: "",
+         imageUrl: "../../../public/images/techblog.png",
          githubUrl: "https://github.com/ruthiepina/MVC-Tech-Blog",
       },
       {
          id: 3,
          title: "Weather Dashboard",
          description: "Click to see the repo",
-         imageUrl: "",
+         imageUrl: "../../../public/images/weatherdash.png",
          githubUrl: "https://github.com/ruthiepina/Weather-Dashboard",
       },
       {
          id: 4,
          title: "Employee Tracker",
          description: "Click to see the repo",
-         imageUrl: "",
+         imageUrl: "../../../public/images/tracker.png",
          githubUrl: "https://github.com/ruthiepina/Employee-Tracker",
       },
       {
          id: 5,
          title: "SVG Logo Generator",
          description: "Click to see the repo",
-         imageUrl: "",
+         imageUrl: "../../../public/images/svg.png",
          githubUrl: "https://github.com/ruthiepina/SVG-Logo-Generator",
       },
       {
          id: 6,
          title: "E-Commerce Backend",
          description: "Click to see the repo",
-         imageUrl: "",
+         imageUrl: "../../../public/images/ecomm.png",
          githubUrl: "https://github.com/ruthiepina/E-Commerce-Back-End",
       },
    ];
@@ -67,7 +67,7 @@ const Portfolio = () => {
          <h1>Portfolio</h1>
          <div className="portfolio-cards">
             {portfolioItems.map((item) => (
-               <a href={item.linkUrl} target="_blank" rel="noopener noreferrer" key={item.id}>
+               <a href={item.githubUrl} target="_blank" rel="noopener noreferrer" key={item.id}>
                   <div className="portfolio-card">
                      <h3>{item.title}</h3>
                      <img src={item.imageUrl} alt={`Item ${item.id}`} />
