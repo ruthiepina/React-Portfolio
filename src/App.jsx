faeae0568526160df233c73df2df24b5e5abf3a5
RMP <ruthiepina@gmail.com>
2024-04-09 20:09:12 -0600

    added json data for my portfolio and about me components. Need to add images for my repos/projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ContactMe from "./components/ContactMe/ContactMe";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Resume from "./components/Resume/Resume.jsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   return (
+      <Router>
+         <div className="App">
+            <Header />
+            <main>
+               <Routes>
+                  <Route path="/" element={<AboutMe />} /> {/* 'About Me' now serves as the Home Page */}
+                  <Route path="/contact" element={<ContactMe />} />
+                  <Route path="/portfolio" element={<Portfolio />} />
+                  <Route path="/resume" element={<Resume />} />
+               </Routes>
+            </main>
+            <Footer />
+         </div>
+      </Router>
+   );
 }
 
-export default App
+export default App;
